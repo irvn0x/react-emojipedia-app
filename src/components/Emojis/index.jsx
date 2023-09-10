984c773575fe835ad475a4a99920e39b6e0fa60d
Irvansyah <ilhamirvansyah.dev@gmail.com>
2023-09-10 10:36:28 +0700

    improve dynamic rendereding emojis components

--- src/components/Emojis/index.jsx
+++ src/components/Emojis/index.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import { filterEmojis } from "../../utils/filterEmojis";
 
 import EmojiBox from "../EmojiBox";
-
+import Empty from "../Empty";
 import styles from "./Emojis.module.css";
 
 const Emojis = ({ emojisData, searchText }) => {
@@ -19,13 +19,17 @@ const Emojis = ({ emojisData, searchText }) => {
     );
   }, [emojisData, searchText]);
 
-  return (
-    <div className={styles.emojisGrid}>
-      {filteredEmojis.map((data, index) => (
-        <EmojiBox key={index} title={data.title} symbol={data.symbol} />
-      ))}
-    </div>
-  );
+  if (filteredEmojis.length > 0) {
+    return (
+      <div className={styles.emojisGrid}>
+        {filteredEmojis.map((data, index) => (
+          <EmojiBox key={index} title={data.title} symbol={data.symbol} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Empty text="Oops, not found. Let's try another keyword!" />;
+  }
 };
 
 Emojis.propTypes = {
